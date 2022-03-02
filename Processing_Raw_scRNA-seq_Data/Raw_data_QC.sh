a732b8d3d313a14d2d0ace99c08105c3a911981a
Yujing Lin <67212190+linyujing199488@users.noreply.github.com>
2022-03-02 21:05:58 +0800

    Update Raw_data_QC.sh

--- Processing_Raw_scRNA-seq_Data/Raw_data_QC.sh
+++ Processing_Raw_scRNA-seq_Data/Raw_data_QC.sh
@@ -4,4 +4,8 @@ wget wget https://sra-pub-run-odp.s3.amazonaws.com/sra/ERR522959/ERR522959
 fastq-dump --split-3 --gzip ERR522959 -O ./
 #fastq-dump --split-3 --defline-qual '+' --defline-seq '@$ac-$si/$ri length=$rl' --gzip SRR921318.sra -O bwd
 
-less Share/
+less Share/ERR522959_1.fastq
+less Share/ERR522959_1.fastq
+ 2. #Solution and Downloading the Report
+mkdir fastqc_results
+fastqc -o fastqc_results/ Share/ERR522959_1.fastq Share/ERR522959_2.fastq
